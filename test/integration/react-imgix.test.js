8be2b1584463095f32e0651e2ac849bb03603ce7
Frederick Fogerty <frederick.fogerty@gmail.com>
2018-06-22 10:04:55 +1200

    WIP

--- test/integration/react-imgix.test.js
+++ test/integration/react-imgix.test.js
@@ -3,14 +3,53 @@ import Imgix from "react-imgix";
 import React from "react";
 import { mount, shallow } from "enzyme";
 
-const src = "http://domain.imgix.net/image.jpg";
+const src = "http://assets.imgix.net/unsplash/lighthouse.jpg";
+let containerDiv;
+beforeEach(() => {
+  containerDiv = global.document.createElement("div");
+  global.document.body.appendChild(containerDiv);
+});
+
+afterEach(() => {
+  global.document.body.removeChild(containerDiv);
+});
+
+const renderIntoContainer = element => {
+  return mount(element, { attachTo: containerDiv });
+};
+
 describe("When in default mode", () => {
-  const renderImage = () => mount(<Imgix src={src} />);
+  const renderImage = () =>
+    renderIntoContainer(
+      <div
+        style={{
+          width: 105,
+          height: 100,
+          display: "flex",
+          alignItems: "stretch"
+        }}
+      >
+        <Imgix
+          src={src}
+          // width={100}
+          // height={100}
+          // style={{ height: 100, width: 100 }}
+        />
+      </div>
+    );
 
   it("an <img> should be rendered", () => {
     expect(renderImage().find("img")).toHaveLength(1);
   });
   it("the rendered element's src should be set", () => {
+    const renderedImage = renderImage();
+    renderedImage.getDOMNode().style.height = "220px";
+    console.log(
+      JSON.stringify(
+        renderedImage.getDOMNode().children[0].getBoundingClientRect()
+      )
+      // .getBoundingClientRect().height
+    );
     expect(
       renderImage()
         .find("img")
