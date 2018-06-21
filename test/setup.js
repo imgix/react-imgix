769c43a15f64697cef60dc7751411dca02bf66d5
Frederick Fogerty <frederick.fogerty@gmail.com>
2018-06-22 10:03:45 +1200

    test: break out setup files into one for each test target

--- test/setup.js
+++ test/setup.js
@@ -1,10 +1,4 @@
+global.expect = require("expect");
 const addExtraJestMatchers = () => require("jest-extended");
 
-import { configure } from "enzyme";
-import Adapter from "enzyme-adapter-react-16";
-const configureEnzymeWithAdapter = () => {
-  configure({ adapter: new Adapter() });
-};
-
 addExtraJestMatchers();
-configureEnzymeWithAdapter();
---
+++ test/setupUnit.js
@@ -0,0 +1,9 @@
+require("./setup");
+
+import { configure } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+const configureEnzymeWithAdapter = () => {
+  configure({ adapter: new Adapter() });
+};
+
+configureEnzymeWithAdapter();
