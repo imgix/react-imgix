5661bb6243127ec3db2d661332e98374f95edd8e
Frederick Fogerty <frederick.fogerty@gmail.com>
2018-09-21 14:45:42 +1200

    feat: add ability to configure warnings

---
+++ src/config.js
@@ -0,0 +1,25 @@
+const config = {
+  warnings: {
+    fallbackImage: true,
+    sizesAttribute: true
+  }
+};
+
+const _setWarning = (name, value) => {
+  if (!name || !(name in config.warnings)) {
+    return;
+  }
+  config.warnings[name] = value;
+};
+
+class PublicConfigAPI {
+  static disableWarning(name) {
+    _setWarning(name, false);
+  }
+  static enableWarning(name) {
+    _setWarning(name, true);
+  }
+}
+
+export default config;
+export { PublicConfigAPI };
