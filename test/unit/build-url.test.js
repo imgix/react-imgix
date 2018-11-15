1113d3b14cc04120902c2b1b44606222dc01d8e6
Frederick Fogerty <frederick.fogerty@gmail.com>
2018-11-15 06:12:10 +0100

    test: add tests for ixlib and disableLibraryParam

--- test/unit/build-url.test.js
+++ test/unit/build-url.test.js
@@ -7,3 +7,30 @@ test("buildURL builds a url correctly", () => {
   expect(actualURL.searchParams.get("w")).toBe("450");
   expect(actualURL.searchParams.get("h")).toBe("100");
 });
+
+test("buildURL includes an ixlib parameter", () => {
+  const actual = buildURL("https://demo.imgix.net/abc.png", { w: 450, h: 100 });
+
+  const actualURL = new URL(actual);
+  expect(actualURL.searchParams.get("ixlib")).toEqual(
+    createIxLibParamMatcher()
+  );
+});
+
+test("ixlib addition can be disable with disableLibraryParam", () => {
+  const actual = buildURL(
+    "https://demo.imgix.net/abc.png",
+    { w: 450, h: 100 },
+    { disableLibraryParam: true }
+  );
+
+  const actualURL = new URL(actual);
+  expect(actualURL.searchParams.has("ixlib")).toBe(false);
+});
+
+const createIxLibParamMatcher = () => {
+  const expectedVersion = require("read-pkg-up").sync().pkg.version;
+  const expectedParam = `react-${expectedVersion}`;
+
+  return expect.stringContaining(expectedParam);
+};
