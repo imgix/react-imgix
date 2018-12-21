17a74dbb3e70b80e919f333fb4693dcdd9dc3054
Frederick Fogerty <frederick.fogerty@gmail.com>
2018-12-21 11:05:19 +0100

    chore: support SSR with background mode

--- src/react-imgix-bg.jsx
+++ src/react-imgix-bg.jsx
@@ -27,7 +27,7 @@ const BackgroundImpl = props => {
   const { w: forcedWidth, h: forcedHeight } = imgixParams;
   const hasDOMDimensions = contentRect.bounds.top != null;
   const htmlAttributes = props.htmlAttributes || {};
-  const dpr = toFixed(2, imgixParams.dpr || window.devicePixelRatio || 1);
+  const dpr = toFixed(2, imgixParams.dpr || global.devicePixelRatio || 1);
   const ref = htmlAttributes.ref;
   const onRef = el => {
     measureRef(el);
