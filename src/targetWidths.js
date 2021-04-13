9e853f43f4bfd10ce5d1d78c091438f593464fe6
Eric Sanchez <esanchez@imgix.com>
2021-04-13 16:02:33 -0700

    feat: impl targetWidths in terms of ImgixClient.targetWidths()

--- src/targetWidths.js
+++ src/targetWidths.js
@@ -1,18 +1,3 @@
-function targetWidths() {
-  const resolutions = [];
-  let prev = 100;
-  const INCREMENT_PERCENTAGE = 8;
-  const MAX_SIZE = 8192;
+import ImgixClient from "@imgix/js-core";
 
-  const ensureEven = (n) => 2 * Math.round(n / 2);
-
-  while (prev <= MAX_SIZE) {
-    resolutions.push(ensureEven(prev));
-    prev *= 1 + (INCREMENT_PERCENTAGE / 100) * 2;
-  }
-
-  resolutions.push(MAX_SIZE);
-  return resolutions;
-}
-
-export default targetWidths();
+export default ImgixClient.targetWidths();
