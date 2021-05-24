c2b555ecaba9da0630ed5149e366bbf09f09bba8
Luis Ball <lball@imgix.com>
2021-05-24 14:54:13 -0400

    feat: wrap ReactImgix with props HOFs
    This commit wraps all the ReactImgix components with the higher order
    functions that process and merge the Providers props with the Component
    props.
    
    This simplifies the usage of the ImgixContext wrapper for the user. As
    the HOF will apply the context and merge its props with the user's imgix
    Component props behind the scenes. In brief, this makes it so the user
    only needs to worry about adding the ImgixContext component.

--- src/HOCs/index.js
+++ src/HOCs/index.js
@@ -1,1 +1,2 @@
 export * from "./shouldComponentUpdateHOC";
+export * from "./imgixProvider"
--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import ReactImgix, { Picture, Source } from "./react-imgix";
 import { PublicConfigAPI } from "./config";
-
 import { buildURLPublic as buildURL } from "./constructUrl";
+import {
+  ImgixProvider,
+} from "./HOCs"
+
+export { ImgixProvider };
 export { buildURL };
-
-export default ReactImgix;
 export { Picture, Source, PublicConfigAPI };
-
 export { Background } from "./react-imgix-bg";
+export default ReactImgix;
