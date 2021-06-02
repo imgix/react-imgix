83e935a6d2adeb9f69fda00ec2f90b115be7fab5
Luis Ball <lball@imgix.com>
2021-06-02 09:55:29 -0400

    test: add imgix-provider merge and noop tests

--- test/unit/imgix-provider.test.jsx
+++ test/unit/imgix-provider.test.jsx
@@ -44,29 +44,66 @@ describe("ImgixProvider", () => {
 
     // ensure Provider value correctly set
     const expectedProps = {
-      children: (
-        <ReactImgix
-          src="https://assets.imgix.net/examples/pione.jpg"
-          sizes="50vw"
-        />
-      ),
-      value: { domain: "sdk-test.imgix.net", sizes: "100vw" },
-    };
+      children: < ReactImgix src="https://assets.imgix.net/examples/pione.jpg" sizes="50vw" />,
+      value: { domain: "sdk-test.imgix.net", sizes: "100vw", }
+    }
 
-    const renderedComponent = shallow(wrappedComponent);
-    expect(renderedComponent.props()).toEqual(expectedProps);
-  });
+    const renderedComponent = shallow(wrappedComponent)
+    expect(renderedComponent.props()).toEqual(expectedProps)
+  })
+
+  test('should merge the Provider and Child props', () => {
+
+    const modifiedProps = { ...imageProps };
+    modifiedProps.src = "examples/pione.jpg"
+    modifiedProps.sizes = null
+
+    const wrappedComponent = (
+      <ImgixProvider {...providerProps}>
+        <ReactImgix {...modifiedProps} />
+      </ImgixProvider>
+    )
+
+    // ensure Provider and Child props are merged as intended
+    const expectedProps = {
+      disableSrcSet: false,
+      domain: "sdk-test.imgix.net",
+      height: null,
+      imgixParams: undefined,
+      onMounted: undefined,
+      sizes: "100vw",
+      src: "https://sdk-test.imgix.net/examples/pione.jpg",
+      width: null,
+    }
+
+    const renderedComponent = mount(wrappedComponent)
+    const renderedProps = renderedComponent
+      .childAt(0) // mergePropsHOF
+      .childAt(0) // processPropsHOF
+      .childAt(0) // ChildComponent
+      .props()
+    // remove noop function that breaks tests
+    renderedProps.onMounted = undefined
+
+    expect(renderedProps).toEqual(expectedProps)
+  })
 
   test('should log error when has no consumers', () => {
-    jest.spyOn(global.console, 'error').mockImplementation(() => {})
+    jest.spyOn(global.console, 'error').mockImplementation(() => { })
 
     const wrappedComponent = (
       <ImgixProvider {...providerProps}>
       </ImgixProvider>
     )
 
-    shallow(wrappedComponent)
+    const expectedProps = {
+      children: undefined,
+      value: providerProps
+    }
 
+    const renderedComponent = shallow(wrappedComponent)
+
+    expect(renderedComponent.props()).toEqual(expectedProps)
     expect(console.error).toHaveBeenCalledWith(
       expect.stringContaining("ImgixProvider must have at least one Imgix child component")
     );
