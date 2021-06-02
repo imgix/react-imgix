febd394044d99e175fcb506f7b89941c49403cbc
Luis Ball <lball@imgix.com>
2021-06-02 08:50:33 -0400

    feat: recursive merge imgixParams & htmlAttributes

--- src/HOFs/propMerger.js
+++ src/HOFs/propMerger.js
@@ -3,29 +3,59 @@ import { useImgixContext } from "../HOCs"
 
 /**
  * Merges the `src` object into the `destination` object. Destination values are
- * overwritten by source values. Source properties that resolve to `undefined` 
- * are skipped if a destination value exists.
- * 
+ * not overwritten by source values. Destination properties that resolve to 
+ * `undefined` or `null` are overwritten if a destination value exists. It 
+ * recursively merges the `imgixParams` and `htmlAttributes` values.
+ *
  * @param {Object} src - The Provider component's props object
  * @param {Object} destination - The child component's props object
  * @returns Object with the combined values from `src` & `destination` Objects
+ *
  * @example
- * let src = {foo: 'bar'}
- * let destination = {foo: 'baz', bar: 'baz'}
- * let result = mergeProps(src, destination)
- * result => {foo: 'bar', bar: 'baz'}
+ *  const src = {
+ *    width: 100,
+ *    height: 200,
+ *    imgixParams: { ar: "1:2", dpr: 2},
+ *    htmlAttributes: { styles: "width: 50" }
+ *  }
+ *  const destination = {
+ *    width: 101,
+ *    height: 201,
+ *    imgixParams: { dpr: 1 },
+ *    htmlAttributes: { styles: "width: 100" }
+ *  }
+ *  const result = mergeProps(src, destination);
+ *
+ *  {
+ *    width: 101,
+ *    height: 201,
+ *    imgixParams: { ar: "1:2", dpr: 1 },
+ *    htmlAttributes: { styles: "width: 100" }
+ *  }
  * 
  */
-export function mergeProps(src = {}, destination = {}) {
-  // ensure the arguments are not modified
-  Object.freeze(src);
-  Object.freeze(destination);
+export const mergeProps = (src, destination) => {
+  if (src == null && destination !== null) {
+    return destination
+  }
+  if (src !== null && destination == null) {
+    return src;
+  }
+  if (src == null && destination == null) {
+    return undefined
+  }
 
   const newProps = { ...destination }
 
   for (const [k, v] of Object.entries(src)) {
-    if (newProps[k] == null) {
-      newProps[k] = v;
+    if (newProps[k] == null && v !== null) {
+      newProps[k] = v
+    }
+    // recursively merge imgixParams and htmlAttributes
+    if (k === "imgixParams" || k === "htmlAttributes") {
+      if (v !== null) {
+        newProps[k] = mergeProps(src[k], newProps[k])
+      }
     }
   }
   return newProps;
@@ -43,7 +73,6 @@ export function mergeProps(src = {}, destination = {}) {
  */
 export const mergeComponentPropsHOF = (Component) => (props) => {
   const contextProps = useImgixContext();
-
   if (contextProps === undefined) {
     return <Component {...props} />
   }
