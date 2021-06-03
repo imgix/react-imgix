523ec8975d26094f96fdbe84615b57c1e5368146
Luis Ball <lball@imgix.com>
2021-06-03 11:50:07 -0400

    chore: prettier and expect undefined not null

--- test/unit/format-props.test.js
+++ test/unit/format-props.test.js
@@ -1,37 +1,36 @@
-import { formatProps } from "../../src/HOFs/propFormatter"
+import { formatProps } from "../../src/HOFs/propFormatter";
 
-describe('formatProps()', () => {
-  it('should set width to null if negative', () => {
-    const props = { height: 100, src: "/pione.jpg", width: -100 }
-    const result = formatProps(props)
-    const expected = { height: 100, src: "/pione.jpg", width: null}
+describe("formatProps()", () => {
+  it("should set width to undefined if negative", () => {
+    const props = { height: 100, src: "/pione.jpg", width: -100 };
+    const result = formatProps(props);
+    const expected = { height: 100, src: "/pione.jpg", width: undefined };
 
-    expect(result).toEqual(expected)
-  })
+    expect(result).toEqual(expected);
+  });
 
-  it('should set height to null if negative', () => {
-    const props = { height: -100, src: "/pione.jpg", width: 100 }
-    const result = formatProps(props)
-    const expected = { height: null, src: "/pione.jpg", width: 100}
+  it("should set height to undefined if negative", () => {
+    const props = { height: -100, src: "/pione.jpg", width: 100 };
+    const result = formatProps(props);
+    const expected = { height: undefined, src: "/pione.jpg", width: 100 };
 
-    expect(result).toEqual(expected)
-  })
+    expect(result).toEqual(expected);
+  });
 
-
-  it('should set src to 2-step URL if domain prop present', () => {
+  it("should set src to 2-step URL if domain prop present", () => {
     const props = {
       domain: "assets.imgix.net",
       src: "/pione.jpg",
-      width: 100
-    }
-    const result = formatProps(props)
+      width: 100,
+    };
+    const result = formatProps(props);
     const expected = {
-      height: null,
+      height: undefined,
       domain: "assets.imgix.net",
       src: "https://assets.imgix.net/pione.jpg",
-      width: 100
-    }
+      width: 100,
+    };
 
-    expect(result).toEqual(expected)
-  })
-})
+    expect(result).toEqual(expected);
+  });
+});
