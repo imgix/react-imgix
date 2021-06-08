0539b1a460d2fd9925b78b3d51c6944e8291e474
Luis Ball <lball@imgix.com>
2021-06-08 14:42:09 -0400

    test: use insecure URL if useHttps set to false

--- test/unit/format-props.test.js
+++ test/unit/format-props.test.js
@@ -17,7 +17,7 @@ describe("formatProps()", () => {
     expect(result).toEqual(expected);
   });
 
-  it("should set src to 2-step URL if domain prop present", () => {
+  it("should set src to 1-step URL if domain prop present", () => {
     const props = {
       domain: "assets.imgix.net",
       src: "/pione.jpg",
@@ -29,6 +29,25 @@ describe("formatProps()", () => {
       domain: "assets.imgix.net",
       src: "https://assets.imgix.net/pione.jpg",
       width: 100,
+    };
+
+    expect(result).toEqual(expected);
+  });
+
+  it("should set src to insecure 1-step URL if useHttp prop false", () => {
+    const props = {
+      domain: "assets.imgix.net",
+      src: "/pione.jpg",
+      width: 100,
+      useHttps: false,
+    };
+    const result = formatProps(props);
+    const expected = {
+      height: undefined,
+      domain: "assets.imgix.net",
+      src: "http://assets.imgix.net/pione.jpg",
+      width: 100,
+      useHttps: false,
     };
 
     expect(result).toEqual(expected);
