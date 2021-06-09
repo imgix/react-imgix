ffd63335314479baaa7ba9f7a15c446b9d4a1f32
Luis Ball <lball@imgix.com>
2021-06-09 16:13:42 -0400

    chore: fix nesting and remove only declaration

--- test/unit/find-closest.test.js
+++ test/unit/find-closest.test.js
@@ -4,10 +4,10 @@ test("when closest value is below the search value", () =>
   expect(findClosest(101, [100, 200])).toBe(100));
 test("when closest value is above the search value", () =>
   expect(findClosest(151, [100, 200])).toBe(200));
-test.only("when closest value is the median of two nearest values, the largest value is returned", () =>
+test("when closest value is the median of two nearest values, the largest value is returned", () =>
   expect(findClosest(150, [100, 200])).toBe(200));
 
-test("Edge cases", () => {
+describe("Edge cases", () => {
   test("when value is smaller than the entire array", () =>
     expect(findClosest(80, [100, 200])).toBe(100));
   test("when value is larger than the entire array", () =>
