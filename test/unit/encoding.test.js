c13e3bc98227c9c623be37c00cc697198b5f3788
Frederick Fogerty <frederick.fogerty@gmail.com>
2022-02-24 19:55:08 +0100

    test: path encoding can be disabled for Source element

--- test/unit/encoding.test.js
+++ test/unit/encoding.test.js
@@ -1,6 +1,6 @@
 import { mount } from "enzyme";
 import React from "react";
-import ReactImgix from "../../src/index";
+import ReactImgix, { Source } from "../../src/index";
 
 const imageProps = {
   src: "https://assets.imgix.net/examples/space bridge.jpg",
@@ -46,5 +46,49 @@ describe("ReactImgix", () => {
     const renderedImageTagProps = renderedComponent.find("img").props();
 
     expect(renderedImageTagProps.srcSet).toMatch(expectedSrc);
+  });
+});
+describe("Source", () => {
+  test("should encode srcSet path", () => {
+    const component = <Source {...imageProps} />;
+
+    const expectedSrc = "https://assets.imgix.net/examples/space%20bridge.jpg";
+
+    const renderedComponent = mount(component);
+    const renderedSourceTagProps = renderedComponent.find("source").props();
+
+    expect(renderedSourceTagProps.srcSet).toMatch(expectedSrc);
+  });
+  test("should not encode the srcSet path if disablePathEncoding is set to true", () => {
+    const component = <Source {...imageProps} disablePathEncoding />;
+
+    const expectedSrc = "https://assets.imgix.net/examples/space bridge.jpg";
+
+    const renderedComponent = mount(component);
+    const renderedSourceTagProps = renderedComponent.find("source").props();
+
+    expect(renderedSourceTagProps.srcSet).toMatch(expectedSrc);
+  });
+  test("should encode the srcset path if disableSrcSet is true", () => {
+    const component = <Source {...imageProps} disableSrcSet />;
+
+    const expectedSrc = "https://assets.imgix.net/examples/space%20bridge.jpg";
+
+    const renderedComponent = mount(component);
+    const renderedSourceTagProps = renderedComponent.find("source").props();
+
+    expect(renderedSourceTagProps.srcSet).toMatch(expectedSrc);
+  });
+  test("should not encode the srcset path if disablePathEncoding and disableSrcSet are set to true", () => {
+    const component = (
+      <Source {...imageProps} disableSrcSet disablePathEncoding />
+    );
+
+    const expectedSrc = "https://assets.imgix.net/examples/space bridge.jpg";
+
+    const renderedComponent = mount(component);
+    const renderedSourceTagProps = renderedComponent.find("source").props();
+
+    expect(renderedSourceTagProps.srcSet).toMatch(expectedSrc);
   });
 });
