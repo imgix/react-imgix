efa0eeb8d6e210da58698e18b806f3884040861c
Luis Ball <lball@imgix.com>
2022-12-13 14:25:18 -0800

    test(background): test disable path encoding

--- test/unit/encoding.test.js
+++ test/unit/encoding.test.js
@@ -1,6 +1,6 @@
 import { mount } from "enzyme";
 import React from "react";
-import ReactImgix, { ImgixProvider, Source } from "../../src/index";
+import ReactImgix, { ImgixProvider, Source, Background } from "../../src/index";
 
 const imageProps = {
   src: "https://assets.imgix.net/examples/space bridge.jpg",
@@ -27,6 +27,33 @@ describe("ReactImgix", () => {
 
     expect(renderedImageTagProps.src).toMatch(expectedSrc);
   });
+
+  test("Background should not encode the src path if disablePathEncoding is set to true", () => {
+    const encodedSrc =
+      "https://sdk-test.imgix.net/%26%24%2B%2C%3A%3B%3D%3F%40%23.jpg";
+    // Force BG to render immediately by giving it w,h params, contentRect bounds
+    // and a measureRef that points to null
+    const component = (
+      <Background
+        className="bg-test"
+        src={encodedSrc}
+        disablePathEncoding
+        disableLibraryParam
+        measureRef={() => null}
+        imgixParams={{ w: 100, h: 100 }}
+        contentRect={{ bounds: { top: 10, width: 100, height: 100 } }}
+      >
+        <div>Content</div>
+      </Background>
+    );
+    const expectedBgUrl = `url(${encodedSrc}?fit=crop&w=100&h=100&dpr=1)`;
+
+    const renderedComponent = mount(component);
+    const renderedBg = renderedComponent.find("div.bg-test").props();
+
+    expect(renderedBg.style.backgroundImage).toMatch(expectedBgUrl);
+  });
+
   test("should encode the srcset", () => {
     const component = <ReactImgix {...imageProps} />;
 
